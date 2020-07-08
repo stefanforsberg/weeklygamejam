fc31b735f4a6b32e79e672026f8ef06935853d6a
stefan.forsberg <cykelkatalogen@popkram.com>
2020-07-08 11:09:50 +0200

    Tutorial level

--- week156 noodle/mainScene.js
+++ week156 noodle/mainScene.js
@@ -4,12 +4,20 @@ class MainScene extends Phaser.Scene {
     }
     
     create() {
+
+        this.labelScene = this.scene.get('LabelScene');
+
+        this.gameState = {
+            levelStarted: false,
+            levelStartTime: 0,
+        }
+
         this.loadingText = this.add.text(30, 30, 'Started game...').setFontFamily('Arial').setFontSize(32).setColor('#ffffff');
 
         this.matter.world.setBounds(120, 636, 508, 445, 26, true, true, false, true);
         this.matter.world.setGravity(0,0.1)
 
-        this.matter.world.pause();
+        
 
         this.startImage = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2, 'kitchen');
 
@@ -21,21 +29,13 @@ class MainScene extends Phaser.Scene {
 
         this.noodle.setInteractive();
 
-        this.noodle.on('pointerdown', () => { this.startGame();}, this);
+        this.noodle.on('pointerdown', this.noodleEvent, this);
 
-        // this.noodle = this.add.image(400, 300, 'noodle');
-        // // this.noodle = this.add.rectangle(300,200,40,40, "#ff00ff", 1)
 
-        // this.physics.add.existing(this.noodle, false);
+        this.clock = this.add.text(315, 162).setFontFamily('Arial').setFontSize(48).setColor('#ffffff');
 
-        this.noodle.angle = 35;
-        this.noodle.setVelocity(0, 1);
-        this.noodle.setBounce(0.3, 0.3);
-
-        this.clock = this.add.text(32, 32).setFontFamily('Arial').setFontSize(48).setColor('#000000');
-        this.clockTime = 0;
-
-        this.clock.setText("Hey");
+        this.resetLevel();
+        
 
         // this.noodle.body.setCollideWorldBounds(true);
         // // this.noodle.body.setBoundsRectangle(new Phaser.Geom.Rectangle(160, 508, 430, 325));
@@ -50,11 +50,52 @@ class MainScene extends Phaser.Scene {
         
     }
 
-    startGame() {
+    resetLevel() {
 
-        this.matter.world.resume();
+        this.noodle.visible = false;
 
-        this.timedEvent = this.time.addEvent({ delay: 1000, callback: this.onEvent, callbackScope: this, loop: true });
+        this.noodle.y = 500;
+
+        this.noodle.angle = 35;
+        this.noodle.setVelocity(0, 1);
+        this.noodle.setBounce(0.3, 0.3);
+
+        this.clockTime = 0;
+        this.scene.pause();
+        this.clock.setText("00:00");
+        this.gameState.levelStarted = false;
+
+        this.level1();
+    }
+
+    noodleEvent() {
+        if(this.gameState.levelStarted) {
+            this.endLevel();
+        } 
+    }
+
+    endLevel() {
+        this.scene.pause();
+        console.log(this.clockRealTimeEvent.getElapsed())
+    }
+
+    startLevel() {
+
+        console.log(this.scene.scene.time.now)
+
+        this.gameState.levelStarted = true;
+
+        this.noodle.visible = true;
+
+        this.scene.resume()
+
+        this.clockRealTimeEvent = this.time.addEvent({ delay: 100000, loop: true });
+
+
+
+        this.clockTimeEvent = this.time.addEvent({ delay: 1000, callback: this.onEvent, callbackScope: this, loop: true });
+
+        
 
         var particles = this.add.particles('bubble');
 
@@ -68,18 +109,53 @@ class MainScene extends Phaser.Scene {
             gravityY: 0,
             scale: { start: 0.2, end: 2 },
             alpha: { start: 0.4, end: 0 },
-            quantity: 2,
+            quantity: 13,
             blendMode: 'ADD',
             emitZone: { type: 'random', source: new Phaser.Geom.Rectangle(140, 1058, 470, 25) },
         }); 
+
+        emitter.depth = 100;
+    }
+
+    addBrocolli() {
+        
+        const brocolli = this.add.image(Phaser.Math.Between(300, 600), 400, 'brocolli');
+
+        var arrow = '0 49 4 18 76 0 107 28 105 61 72 76 59 94 45 94';
+        this.matter.add.gameObject(brocolli, { shape: { type: 'fromVerts', verts: arrow, flagInternal: true } });
+
+        brocolli.alpha = 0.6
+        brocolli.angle = 35;
+        brocolli.setVelocity(0, 1);
+        brocolli.setAngularVelocity(0.1);
+        brocolli.setBounce(0.3, 0.3);
+        
+        // const brocolli = this.matter.add.image(400, 300, 'brocolli');
+
+        // brocolli.angle = 35;
+        // brocolli.setVelocity(0, 1);
+        // brocolli.setBounce(0.3, 0.3);
+    }
+
+    addTimeDouble() {
+        
+        const timedouble = this.add.image(500, 185, 'timedouble');
+
+        this.clockTimeEvent.timeScale = 2;
+        
+        // const brocolli = this.matter.add.image(400, 300, 'brocolli');
+
+        // brocolli.angle = 35;
+        // brocolli.setVelocity(0, 1);
+        // brocolli.setBounce(0.3, 0.3);
     }
 
     onEvent() {
         this.clockTime += 1;
 
-        if(this.clockTime > 5) {
-            this.timedEvent.timeScale = 2;
-        }
+        // if(this.clockTime > 5) {
+        //     this.timedEvent.timeScale = 2;
+        // }
 
         var m = Math.floor(this.clockTime % 3600 / 60);
         var s = Math.floor(this.clockTime % 3600 % 60);
@@ -87,6 +163,30 @@ class MainScene extends Phaser.Scene {
 
 
         this.clock.setText(`${m < 10 ? "0" + m : m }:${s < 10 ? "0" + s : s }`);
+    }
+
+
+    level1() {
+        this.gameState.level = {
+            endTime: 14000,
+            cookTimeText: "Cook for 00:20"
+        }
+
+        this.labelScene.showTextBox("Welcome to the tutorial level!\nYour goal here is to cook the noodles for 20 seconds. Click this box to start.", () => {this.startLevel()})
+
+        this.time.addEvent({ delay: 4000, callback: () => { this.scene.pause(); this.labelScene.showTextBox("Above you can see a clock that (usually) show how much time has passed. Click here to to continue", () => {this.scene.resume()}) }, callbackScope: this, loop: false });
+
+        this.time.addEvent({ delay: 7000, callback: this.addBrocolli, callbackScope: this, loop: false });
+        
+        this.time.addEvent({ delay: 10000, callback: () => { this.scene.pause(); this.labelScene.showTextBox("Oh no, evil forces have added a brocolli. Each vegetable will add 3 seconds to the cook timer so now the noodles will need 20 + 3 seconds to finish!", () => {this.scene.resume()}) }, callbackScope: this, loop: false });
+
+        this.time.addEvent({ delay: 15000, callback: () => { this.addTimeDouble(); this.scene.pause(); this.labelScene.showTextBox("What's this? The time bandits have sped up the clock so it moves twice as fast so from this point on 2 seconds on the clock is 1 second in real life. Oh my!", () => {this.scene.resume()}) }, callbackScope: this, loop: false });
+
+        this.time.addEvent({ delay: 21000, callback: () => { this.scene.pause(); this.labelScene.showTextBox("Noodles should be ready in about 2 seconds. To finish the level click the noodles and you will be scored on how close you were to the target cook time.", () => {this.scene.resume()}) }, callbackScope: this, loop: false });
+
+
+        // this.time.addEvent({ delay: 3000, callback: this.addBrocolli, callbackScope: this, loop: false });
+        // this.time.addEvent({ delay: 4000, callback: this.addBrocolli, callbackScope: this, loop: false });
     }
 
 
--- week156 noodle/startScene.js
+++ week156 noodle/startScene.js
@@ -10,26 +10,60 @@ class StartScene extends Phaser.Scene {
         this.load.image('kitchen', 'kitchen.png');
         this.load.image('noodle', 'noodle.png');
         this.load.image('bubble', 'bubble.png');
+        this.load.image('brocolli', 'brocolli.png');
+        this.load.image('timedouble', 'timedouble.png');
 
-        // this.load.audio('theme', [
-        //     'assets/09.ogg',
-        //     'assets/09.mp3'
-        // ]);
-
-        this.startNormalGameButton = this.add.rectangle(100,100,300,300, "#ffffff", 1)
-        this.startNormalGameButton.setInteractive();
-        this.startNormalGameButton.on('pointerdown', () => {this.startGame(false)}, this);
-
+        
     }
 
     create() {
         
         this.loadingText.visible = false;
+
+        this.startNormalGameButton = this.add.rectangle(100,100,300,300, "#ffffff", 1)
+        this.startNormalGameButton.setInteractive();
+        this.startNormalGameButton.on('pointerdown', () => {this.startGame(false)}, this);
     }
+
+
 
     startGame() {
+        
+
+        const labelScene = this.scene.launch('LabelScene');
         this.scene.start('MainScene');
+
+        
     }
 
 
+}
+
+class LabelScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'LabelScene' });
+    }
+
+    create() {
+        
+        
+        
+    }
+
+    showTextBox(text, callBack) {
+
+        var container = this.add.container(120, 260);
+
+        var background = this.add.rectangle(0,0,510,300, "#000000", 0.5).setOrigin(0)
+        
+        const textBox = this.add.text(255, 150, text, { fontFamily: 'Arial', color: '#ffffff', wordWrap: { width: 490 }, fontSize: '30px', align: 'center' }).setOrigin(0.5);
+
+        background.setInteractive();
+
+        container.add(background);
+        container.add(textBox);
+        
+
+        background.on('pointerdown', () => {container.destroy(); callBack()}, this);
+    }
 }
